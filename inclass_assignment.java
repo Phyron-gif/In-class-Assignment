4d172e726339d01bef6105772976dd9051b6e943
Tracy Phyron <tracy.jinor@ashesi.edu.gh>
2025-09-29 18:51:10 +0000

    checkpoint3

---
+++ inclass_assignment.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class inclass_assignment {
+    public static void main(String[] args){
+        Scanner backward = new Scanner(System.in);
+        System.out.println("enter any 4 digit number of choice");
+        int number = backward.nextInt();
+
+       
+
+
+    }
+}
